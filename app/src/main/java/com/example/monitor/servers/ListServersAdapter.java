3d54539861a99b5ab6de1a3b2fcc94cdbc2650ba
lotr <rozzli@mail.ru>
2016-07-17 15:17:07 +0300

    commit

--- app/src/main/java/com/example/monitor/servers/ListServersAdapter.java
+++ app/src/main/java/com/example/monitor/servers/ListServersAdapter.java
@@ -9,8 +9,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.lotr.steammonitor.app.R;
-import com.example.monitor.model.Server;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,10 +19,19 @@ import java.util.List;
 
 public class ListServersAdapter extends RecyclerView.Adapter<ListServersAdapter.Holder> {
 
-    private List<Server> mData;
+    private static final String TAG = "list_servers_adapter";
 
-    public ListServersAdapter(List<Server> data){
-        mData = data;
+    private List<Server> mData;
+    private FavoriteServersPresenter mPresenter;
+
+    public ListServersAdapter(FavoriteServersPresenter presenter){
+        mData = new ArrayList();
+        mPresenter = presenter;
+    }
+
+    void setData(List<Server> data){
+        mData.clear();
+        mData.addAll(data);
     }
 
     @Override
@@ -35,7 +44,7 @@ public class ListServersAdapter extends RecyclerView.Adapter<ListServersAdapter.
     @Override
     public void onBindViewHolder(Holder holder, int position) {
         Server server = mData.get(position);
-        holder.bindDoctor(server);
+        holder.bindDoctor(server, position);
     }
 
     @Override
@@ -44,6 +53,8 @@ public class ListServersAdapter extends RecyclerView.Adapter<ListServersAdapter.
     }
 
     class Holder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private int mPosition;
 
         private TextView mServerName;
         private TextView mServerIp;
@@ -59,21 +70,33 @@ public class ListServersAdapter extends RecyclerView.Adapter<ListServersAdapter.
             mPlayersCount = (TextView)itemView.findViewById(R.id.players);
             mMapName = (TextView)itemView.findViewById(R.id.map_name);
             mButtonDel = (ImageView)itemView.findViewById(R.id.del_item_button);
+            mButtonDel.setOnClickListener(this);
             itemView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
-            Log.i("CLICK_LIST_ITEM", String.valueOf(v.getId()));
+            switch(v.getId()){
+                case R.id.card_view:
+                    mPresenter.onClickListItem();
+                    break;
+                case R.id.del_item_button:
+                    int pos = getLayoutPosition();
+                    Log.i(TAG, "position " + pos);
+                    ListServersAdapter.this.notifyItemRemoved(pos);
+                    mPresenter.onClickDelButton(pos);
+                    //ListServersAdapter.this.notifyDataSetChanged();
+                    //mData.remove(mPosition);
+                    break;
+            }
         }
 
-
-        public void bindDoctor(Server server) {
+        public void bindDoctor(Server server, int position) {
+            mPosition = position;
             mServerName.setText(server.getSrvName());
             mServerIp.setText(server.getIpAddr());
             mPlayersCount.setText(server.getNumPlayers() + "/" + server.getMaxPlayers());
             mMapName.setText(server.getMap());
-           // mButtonDel = (ImageView)itemView.findViewById(R.id.del_item_button);
         }
     }
 }
